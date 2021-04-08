3584fa58a0dbd7cd31676d8a311abe3142ec952c
saadkhan29 <57666391+saadkhan29@users.noreply.github.com>
2021-04-08 15:54:29 +0500

    check point 4

--- routes/article.js
+++ routes/article.js
@@ -4,6 +4,12 @@ const express = require('express');
 
 const router = express.Router();
 
+// Import Model
+const Article = require("../models/Article");
+
+// Grab the form data
+router.use(express.urlencoded({extended: true}));
+
 // HTTP GET - Retrive data from the database - R
 // HTTP POST - To send the data into the database - C
 // HTTP PUT - TO update the data into the database - U
@@ -13,6 +19,23 @@ const router = express.Router();
 // HTTP GET - Load an Article Form
 router.get("/article/add", (req, res) => {
     res.render("article/add");
+})
+
+// HTTP POST - To post the Article
+router.post("/article/add", (req, res) => {
+
+    let article = new Article(req.body);
+
+    // Save the data to the database
+    article.save()
+    .then(() => {
+        res.redirect("/article/index");
+    })
+    .catch((err) => {
+        console.log(err);
+        res.send("ERROR!!!");
+    })
+
 })
 
 module.exports = router;
